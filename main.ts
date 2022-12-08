a40a9fb7568881de3b36fbe77f1278c4118e6dd3
tomi34 <66636695+tomi34@users.noreply.github.com>
2022-12-08 12:07:08 +0100

    main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,1 +1,17 @@
-
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    for (let index = 0; index <= 9; index++) {
+        basic.showNumber(index + 5)
+        basic.pause(100)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    for (let index = 0; index <= 9; index++) {
+        basic.showNumber((index + 1) * 2)
+    }
+})
+for (let index = 0; index <= 9; index++) {
+    basic.showNumber(index)
+    basic.pause(100)
+}
